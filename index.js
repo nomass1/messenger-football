89d5dd5f3b252a7523f9fbde5bfe1428862521cd
Henrique <henriquecolini@gmail.com>
2019-10-04 23:09:13 -0300

    Added framerate and clicking

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 var minRatio = 3 / 4;
-var scale = 0;
+var unit = 0;
+var Vector = /** @class */ (function () {
+    function Vector(x, y) {
+        this.x = x ? x : 0;
+        this.y = y ? y : 0;
+    }
+    Vector.prototype.add = function (vector2) {
+        return new Vector(this.x + vector2.x, this.y + vector2.y);
+    };
+    Vector.prototype.sub = function (vector2) {
+        return new Vector(this.x - vector2.x, this.y - vector2.y);
+    };
+    Vector.prototype.mult = function (factor) {
+        return new Vector(this.x * factor, this.y * factor);
+    };
+    Vector.prototype.div = function (divisor) {
+        return new Vector(this.x / divisor, this.y / divisor);
+    };
+    Vector.prototype.sqrMagnitude = function () {
+        return (this.x * this.x) + (this.y * this.y);
+    };
+    Vector.prototype.magnitude = function () {
+        return Math.sqrt(this.sqrMagnitude());
+    };
+    Vector.prototype.normalized = function () {
+        return this.div(this.magnitude());
+    };
+    return Vector;
+}());
+var Ball = /** @class */ (function () {
+    function Ball(radius) {
+        this.isMoving = false;
+        this.radius = radius;
+    }
+    Ball.prototype.resetPosition = function () {
+        this.position = new Vector(0.5, (canvas.height / unit) - this.radius);
+    };
+    return Ball;
+}());
+var ball = new Ball(0.5 / 3);
 function updateCanvasSize() {
     var w = document.body.clientWidth;
     var h = document.body.clientHeight;
@@ -12,16 +51,47 @@ function updateCanvasSize() {
     else {
         canvas.width = w;
     }
-    scale = canvas.width / 3;
+    unit = canvas.width;
+    if (!ball.isMoving) {
+        ball.resetPosition();
+    }
     draw();
+}
+function handleClick(pos) {
+    if (pos.sub(ball.position).sqrMagnitude() < (ball.radius * ball.radius)) {
+    }
+}
+function getCursorPosition(event) {
+    var rect = canvas.getBoundingClientRect();
+    var x = event.clientX - rect.left;
+    var y = event.clientY - rect.top;
+    return new Vector(x / unit, y / unit);
 }
 function draw() {
     context.fillStyle = "white";
     context.fillRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = "blue";
+    context.beginPath();
+    context.arc(ball.position.x * unit, ball.position.y * unit, ball.radius * unit, 0, 2 * Math.PI);
+    context.closePath();
+    context.fill();
+}
+function update(deltaTime) {
+    draw();
 }
 function start() {
     updateCanvasSize();
-    draw();
+    var last = undefined;
+    var frame = function (timeStamp) {
+        var curr = timeStamp;
+        update(last ? (curr - last) / 1000 : 1 / 60);
+        last = curr;
+        window.requestAnimationFrame(frame);
+    };
+    window.requestAnimationFrame(frame);
 }
 window.addEventListener("resize", updateCanvasSize);
+canvas.addEventListener('mousedown', function (e) {
+    handleClick(getCursorPosition(e));
+});
 start();
--- index.ts
+++ index.ts
@@ -1,7 +1,66 @@
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const context = canvas.getContext("2d");
 const minRatio = 3/4;
-let scale = 0;
+let unit = 0;
+
+class Vector {
+	
+	public x: number;
+	public y: number;
+	
+	constructor(x?: number, y?: number) {
+		this.x = x ? x : 0;
+		this.y = y ? y : 0;
+	}
+
+	public add(vector2: Vector): Vector {
+		return new Vector(this.x + vector2.x, this.y + vector2.y);
+	}
+
+	public sub(vector2: Vector): Vector {
+		return new Vector(this.x - vector2.x, this.y - vector2.y);
+	}
+
+	public mult(factor: number): Vector {
+		return new Vector(this.x * factor, this.y * factor);
+	}
+
+	public div(divisor: number): Vector {
+		return new Vector(this.x / divisor, this.y / divisor);
+	}
+
+	public sqrMagnitude(): number {
+		return (this.x * this.x) + (this.y * this.y);
+	}
+
+	public magnitude(): number {
+		return Math.sqrt(this.sqrMagnitude());
+	}
+
+	public normalized(): Vector {
+		return this.div(this.magnitude());
+	}
+
+}
+
+class Ball {
+
+	public position: Vector;
+	public velocity: Vector;
+	public radius: number;
+	public isMoving = false;
+
+	constructor(radius: number) {
+		this.radius = radius;
+	}
+
+	public resetPosition() {		
+		this.position = new Vector(0.5,(canvas.height/unit)-this.radius);
+	}
+
+}
+
+const ball = new Ball(0.5/3);
 
 function updateCanvasSize() {
 
@@ -16,10 +75,29 @@ function updateCanvasSize() {
 		canvas.width = w;
 	}
 
-	scale = canvas.width/3;
+	unit = canvas.width;
+
+	if (!ball.isMoving) {
+		ball.resetPosition();
+	}
 
 	draw();
 
+}
+
+function handleClick(pos: Vector) {
+
+	if (pos.sub(ball.position).sqrMagnitude() < (ball.radius * ball.radius)) {
+		
+	}
+
+}
+
+function getCursorPosition(event: MouseEvent): Vector {
+	const rect = canvas.getBoundingClientRect();
+	const x = event.clientX - rect.left;
+	const y = event.clientY - rect.top;
+	return new Vector(x/unit, y/unit);
 }
 
 function draw() {
@@ -27,13 +105,38 @@ function draw() {
 	context.fillStyle = "white";
 	context.fillRect(0,0, canvas.width, canvas.height);
 
+	context.fillStyle = "blue";
+	context.beginPath();
+	context.arc(ball.position.x * unit,ball.position.y * unit,ball.radius * unit,0,2*Math.PI)
+	context.closePath();
+	context.fill();
+
 }
 
-function start() {
-	updateCanvasSize();
+function update(deltaTime: number) {	
 	draw();
 }
 
+function start() {
+
+	updateCanvasSize();
+	
+	let last = undefined;
+	const frame = (timeStamp: DOMHighResTimeStamp) => {
+		let curr = timeStamp;
+		update(last ? (curr-last)/1000 : 1/60);
+		last = curr;
+		window.requestAnimationFrame(frame);
+	}
+
+	window.requestAnimationFrame(frame);
+
+}
+
 window.addEventListener("resize", updateCanvasSize);
+
+canvas.addEventListener('mousedown', function(e) {
+    handleClick(getCursorPosition(e));
+});
 
 start();
